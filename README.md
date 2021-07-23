8baf366d57ba998411da2da38f3196a6976bdf58
Phat <phat.huynhtranson@gmail.com>
2021-07-23 09:00:20 -0600

    Add basic structure for liner regression

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 This project is a small one written in HTML, CSS and JS (with d3.js library) in order to illutrate common algorithms in Machine Learning, namely: Linear Regression, Logistic Regression and (hopefully) Principal Component Analysis.
 
 ## Progress
-- [ ] Initialize the project
-- [ ] Create basic site layout
+- [x] Initialize the project
+- [x] Create basic site layout
 - [ ] Create Linear Regression algoritm illustration
 - [ ] Add text description of Linear Regression
 - [ ] Create Logistic Regression algoritm illustration
--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-console.log("Hello World");
+//MVCish (i don't know if this is even legal)
+var view = {
+    width: 350,
+    height: 350,
+    scaleY: d3.scaleLinear().domain([350, 0]).range([0, 350]),
+    reverseScaleY: d3.scaleLinear().domain([0, 350]).range([350, 0]),
+    init() {
+        //Create svg element to hold graph
+        this.svg = d3.select(".linear-graph")
+            .append("svg")
+            .attr("width", this.width)
+            .attr("height", this.height);
+
+        //Create rectangle
+        this.rect = this.svg.append("rect")
+            .attr("width", this.width)
+            .attr("height", this.height)
+            .attr("fill", "#ffffff");
+
+        //Attach listener
+    },
+    setOnClick(onClickListener) {
+        const scaleY = this.scaleY;
+        this.rect.on("click", function handleClick(event) {
+            const coords = d3.pointer(event)
+            const newCoords = [coords[0], scaleY(coords[1])];
+            onClickListener(newCoords);
+        });
+    },
+    appendPointToCanvas(point) {
+        //Flip y scale, 
+        const x = point[0];
+        const y = this.reverseScaleY(point[1]);
+
+        //Append point
+        this.svg.append("circle")
+            .attr("cx", x)
+            .attr("cy", y)
+            .attr("r", "10px")
+            .style("fill", "#E63946");
+    },
+    appendPointToList(point) {
+        
+    }
+
+}
+
+var controller = {
+    init() {
+        view.init();
+        view.setOnClick(function onViewClicked(mousePos) {
+            view.appendPointToCanvas(mousePos);
+        });
+    }
+}
+
+var model = {
+
+}
+
+//Run
+controller.init();
